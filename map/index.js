0873486a21f0a80e88be8207b633d341cacb7ffc
yellowsea <yunhai0644@163.com>
2022-08-25 00:45:31 +0800

    字典学习

---
+++ map/index.js
@@ -0,0 +1,36 @@
+
+// Map  字典 
+
+
+const map = new Map();
+
+
+// Map 的操作 
+
+
+// 添加属性  
+// set(key, value)  使用 key -> value 的方式 添加数据  
+map.set("a", "aa")
+map.set('b', "bb")
+
+
+
+// 插值 字典 中的值 
+// 根据 key 查找 value 
+// get() 
+
+map.get("a")
+
+
+
+// 删除操作 
+// delete()  根据 key 删除 value 
+map.delete('b')
+
+// 还有一种删除方法，  clear()  删除字典全部 内容 
+// map.clear() 
+
+
+
+// 修改数据 
+map.set("a", "aaaaaaaa")
